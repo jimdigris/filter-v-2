7720c945d7790c25979af8bbe0fa2302dd660eb3
jimdirgris <34837142+jimdigris@users.noreply.github.com>
2020-06-15 17:36:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -52,11 +52,50 @@ function getGoods() {
 // --- фильтрация ---
 (function () {
     const FILTER_BUTTON = document.querySelector('#filterButton');
+    const FILTER_RESULTS = document.querySelector("#filterResult");
 
     FILTER_BUTTON.addEventListener('click', onFilterButtonClick);
 
     function onFilterButtonClick() {
-        console.log(getFilters());
-        console.log(getGoods());
+        let goods = getGoods();                                 // получим список товаров с их параметрами
+        let filteringOption = getFilters();                     // получим отмеченные фильтры
+        let result = performFiltering(goods, filteringOption);  // выполним и получим результаты фильтрации
+
+        if (result.length === 0) {
+            FILTER_RESULTS.innerHTML = 'Ничего не найдено. Повторите фильтрацию с другими параметрами.';
+        } else {
+            FILTER_RESULTS.innerHTML = result;
+        }
+    }
+
+    function performFiltering(goods, filteringOption) {
+        let filteredProducts = [];
+
+        goods.forEach(function (item) {                          // берем товар (перебираем)
+            for (let propertyGoods in item) {                    // берем название его свойства (перебираем)
+                if (propertyGoods !== 'id') {                    // название "id" не проверяем
+
+                    for (let prorertyFilters in filteringOption) {      // берем название фильтра (перебираем)
+                        if (prorertyFilters === propertyGoods) {        // если название фильтра совпадает с названием свойства товара
+
+                            for (let valueProperty of filteringOption[prorertyFilters]) {       // берем значение свойства фильтра (перебираем)
+                                if (valueProperty === item[propertyGoods]) {                    // если значение свойства фильтра = значениию свойства товара
+
+                                    if (!filteredProducts.includes(item.id)) {                  // товар проходит фильтрацию, добавляем его id, если   
+                                        filteredProducts.push(item.id);                         // его еще нет в списке
+                                        break;
+                                    }
+
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        });
+
+        return filteredProducts;
     }
 })();
