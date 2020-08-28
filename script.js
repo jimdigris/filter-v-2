9f5e72fa4ed4e6e8c2999a67df7dc79a54abd440
Aleksey Polyakov <jimdigris@mail.ru>
2020-08-28 15:59:21 +0300

    dff

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
 'use strict';
 
+function getProducts() {
+    const ELEMENTS = Array.from(document.querySelector('#products').querySelectorAll('.product'));
+    let products = [];
+
+    ELEMENTS.forEach(function (element) {
+        let product = {
+            id: element.querySelector('.id').getAttribute('data-id'),
+            color: element.querySelector('.color').getAttribute('data-color'),
+            size: element.querySelector('.size').getAttribute('data-size'),
+            direction: element.querySelector('.direction').getAttribute('data-direction'),
+            massa: element.querySelector('.massa').getAttribute('data-massa'),
+            strength: element.querySelector('.strength').getAttribute('data-strength'),
+            flexibility: element.querySelector('.flexibility').getAttribute('data-flexibility'),
+        };
+        products.push(product);
+    });
+
+    console.log(products);
+    return products;
+}
+
+function getFiltres() {
+    const ELEMENTS = document.querySelector('#filters');
+    let filters = {
+        id: getFilterCharacteristic('id'),
+        color: '',
+        size: '',
+        direction: '',
+        massa: '',
+        strength: '',
+        flexibility: ''
+    };
+
+    function getFilterCharacteristic(filterName) {
+        let characteristics = [];
+        //let elements = ELEMENTS.querySelectorAll(filterName);
+
+
+        return characteristics;
+    }
+
+
+
+    console.log(ELEMENTS);
+    return filters;
+}
+
+
+getProducts();
+getFiltres();
